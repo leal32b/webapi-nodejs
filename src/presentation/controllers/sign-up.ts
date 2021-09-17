efb4960be58672ace07c81a459e2d5624b023395
leal32b <leal32b@hotmail.com>
2021-09-17 12:50:19 -0300

    feat: ensure SignUpController returns 400 if passwordConfirmation fails

--- src/presentation/controllers/sign-up.ts
+++ src/presentation/controllers/sign-up.ts
@@ -23,6 +23,10 @@ export class SignUpController implements Controller {
         }
       }
 
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
 
       if (!isValid) {
