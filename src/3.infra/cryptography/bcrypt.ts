fea840811ddea9786aee9659b8525fb795747565
leal32b <leal32b@hotmail.com>
2021-09-18 01:01:27 -0300

    feat: ensure BcryptAdapter return a hash on success

--- src/3.infra/cryptography/bcrypt.ts
+++ src/3.infra/cryptography/bcrypt.ts
@@ -5,8 +5,8 @@ export class BcryptAdapter implements Encrypter {
   constructor (private readonly salt: number) {}
 
   async encrypt (value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt)
+    const hash = await bcrypt.hash(value, this.salt)
 
-    return null
+    return hash
   }
 }
