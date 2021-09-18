b9d9cd53a2acaa791567b3f2ee0404f9cf963f52
leal32b <leal32b@hotmail.com>
2021-09-18 00:10:34 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- test/1.application/usecases/db-add-account.spec.ts
+++ test/1.application/usecases/db-add-account.spec.ts
@@ -91,4 +91,19 @@ describe('DbAddAccount Usecase', () => {
       password: 'hashed_password'
     })
   })
+
+  it('should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest
+      .spyOn(addAccountRepositoryStub, 'add')
+      .mockRejectedValueOnce(new Error())
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+
+    await expect(promise).rejects.toThrowError()
+  })
 })
