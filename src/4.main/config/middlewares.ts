31314bfd1869a400aad0ad951c2259a93de9db66
leal32b <leal32b@hotmail.com>
2021-09-18 17:16:52 -0300

    feat: ensure requests return json as default content type

--- src/4.main/config/middlewares.ts
+++ src/4.main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { bodyParser } from '@/4.main/middlewares/body-parser'
+import { contentTypes } from '@/4.main/middlewares/content-type'
 import { cors } from '@/4.main/middlewares/cors'
 import { Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentTypes)
 }
---
+++ test/4.main/middlewares/content-type.test.ts
@@ -0,0 +1,23 @@
+import app from '@/4.main/config/app'
+import request from 'supertest'
+
+describe('Content Type Middleware', () => {
+  it('should return default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  it('should return xml content type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
