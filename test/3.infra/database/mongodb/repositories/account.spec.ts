d111d26fc47edb5c3a4738e005aa655f8fcdb860
leal32b <leal32b@hotmail.com>
2021-09-18 12:08:43 -0300

    test: ensure AccountMongodbRepository returns an account on success

---
+++ test/3.infra/database/mongodb/repositories/account.spec.ts
@@ -0,0 +1,39 @@
+import { MongodbHelper } from '@/3.infra/database/mongodb/helpers/mongodb-helper'
+import { AccountMongodbRepository } from '@/3.infra/database/mongodb/repositories/account'
+
+interface SutTypes {
+  sut: AccountMongodbRepository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AccountMongodbRepository()
+
+  return {
+    sut
+  }
+}
+
+describe('Account Mongodb Repository', () => {
+  beforeAll(async () => {
+    await MongodbHelper.connect(global.__MONGO_URI__)
+  })
+
+  afterAll(async () => {
+    await MongodbHelper.close()
+  })
+
+  it('should return an account on success', async () => {
+    const { sut } = makeSut()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
