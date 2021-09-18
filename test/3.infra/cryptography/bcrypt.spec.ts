a22b0e376dce2355cb196f61bcdfdbc5c751d849
leal32b <leal32b@hotmail.com>
2021-09-18 01:01:47 -0300

    test: ensure BcryptAdapter return a hash on success

--- test/3.infra/cryptography/bcrypt.spec.ts
+++ test/3.infra/cryptography/bcrypt.spec.ts
@@ -1,6 +1,12 @@
 import { BcryptAdapter } from '@/3.infra/cryptography/bcrypt'
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await Promise.resolve('hashed_value')
+  }
+}))
+
 const salt = 12
 
 interface SutTypes {
@@ -22,5 +28,12 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value')
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  it('should return a hash on success', async () => {
+    const { sut } = makeSut()
+    const hash = await sut.encrypt('any_value')
+
+    expect(hash).toBe('hashed_value')
   })
 })
