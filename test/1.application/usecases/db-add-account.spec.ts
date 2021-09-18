1cb0cb63c1e94b29eff12e26beb4b507cbaec3c7
leal32b <leal32b@hotmail.com>
2021-09-18 00:20:17 -0300

    test: ensure DbAddAccount returns an account on success

--- test/1.application/usecases/db-add-account.spec.ts
+++ test/1.application/usecases/db-add-account.spec.ts
@@ -106,4 +106,21 @@ describe('DbAddAccount Usecase', () => {
 
     await expect(promise).rejects.toThrowError()
   })
+
+  it('should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+
+    await expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
