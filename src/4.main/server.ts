a7bc72b73bb7f8966a33fc72ca8f6e3100cfa223
leal32b <fernando.oliveira@gupy.com.br>
2021-12-16 23:35:43 -0300

    chore: adjust .env values

--- src/4.main/server.ts
+++ src/4.main/server.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config'
 import 'module-alias/register'
-import MongodbAdapter from '@/3.infra/databases/mongodb/adapter/mongodb'
+import { MongodbAdapter } from '@/3.infra/databases/mongodb/adapter/mongodb'
 import app from '@/4.main/config/app'
 
 const PORT = process.env.PORT
-const DATABASE_URL = process.env.DATABASE_MONGODB_HOST
+const DATABASE_URL = process.env.DB_MONGODB_HOST
 
 MongodbAdapter.connect(DATABASE_URL).then(async () => {
   app.listen(PORT, () => {
