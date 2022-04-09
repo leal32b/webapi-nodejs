3e13955b616d994651a0600cddcdeb7da73be52f
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2022-04-09 09:31:10 -0300

    fix: permissions of docker-postgres.sh

--- docker-postgres.sh
+++ docker-postgres.sh
@@ -3,8 +3,9 @@
 set -e
 set -u
 
-local database="${POSTGRES_DB}_test"
+database="${POSTGRES_DB}_test"
 echo "Creating database '$database'"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE DATABASE $database;
   GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
