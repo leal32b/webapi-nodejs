d0f1e850368ea3ac9f70941514bec86e40f91847
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2022-07-03 07:46:35 -0300

    feat: adjust message of not-empty error

--- src/0.domain/errors/not-empty.ts
+++ src/0.domain/errors/not-empty.ts
@@ -2,8 +2,10 @@ import DomainError from '@/0.domain/base/domain-error'
 
 export default class NotEmptyError extends DomainError {
   constructor (field: string, input: string) {
+    const adjustedInput = input === '' ? 'empty' : input
+
     super({
-      message: 'should not be empty',
+      message: `should not be ${adjustedInput}`,
       field,
       input
     })
--- test/0.domain/errors/not-empty.unit.test.ts
+++ test/0.domain/errors/not-empty.unit.test.ts
@@ -12,7 +12,27 @@ const makeSut = (): SutTypes => {
 
 describe('NotEmptyError', () => {
   describe('success', () => {
-    it('returns a NotEmptyError', () => {
+    it('returns a NotEmptyError when input is empty', () => {
+      const { sut } = makeSut()
+      const field = 'any_field'
+      const input = ''
+
+      const result = new sut(field, input)
+
+      expect(result).toBeInstanceOf(NotEmptyError)
+    })
+
+    it('returns the correct message when input is empty', () => {
+      const { sut } = makeSut()
+      const field = 'any_field'
+      const input = ''
+
+      const result = new sut(field, input)
+
+      expect(result.props.message).toBe('should not be empty')
+    })
+
+    it('returns a NotEmptyError when input is null', () => {
       const { sut } = makeSut()
       const field = 'any_field'
       const input = null
@@ -22,14 +42,34 @@ describe('NotEmptyError', () => {
       expect(result).toBeInstanceOf(NotEmptyError)
     })
 
-    it('returns a NotEmptyError with correct message', () => {
+    it('returns the correct message when input is null', () => {
       const { sut } = makeSut()
       const field = 'any_field'
       const input = null
 
       const result = new sut(field, input)
 
-      expect(result.props.message).toBe('should not be empty')
+      expect(result.props.message).toBe('should not be null')
+    })
+
+    it('returns a NotEmptyError when input is undefined', () => {
+      const { sut } = makeSut()
+      const field = 'any_field'
+      const input = undefined
+
+      const result = new sut(field, input)
+
+      expect(result).toBeInstanceOf(NotEmptyError)
+    })
+
+    it('returns the correct message when input is undefined', () => {
+      const { sut } = makeSut()
+      const field = 'any_field'
+      const input = undefined
+
+      const result = new sut(field, input)
+
+      expect(result.props.message).toBe('should not be undefined')
     })
   })
 })
