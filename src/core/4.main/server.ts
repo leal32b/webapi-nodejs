6c0ad6be0c3af638eb5ceca347a43876ac7e0c40
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2022-09-07 08:13:23 -0300

    refactor: adjust server.ts

--- src/core/4.main/server.ts
+++ src/core/4.main/server.ts
@@ -6,15 +6,15 @@ import { defaultDataSource } from '@/core/3.infra/persistence/postgres/data-sour
 import { config } from '@/core/4.main/config/config'
 import { setRouters } from '@/core/4.main/config/set-routers'
 
-const bootstrap = async (): Promise<void> => {
-  const PORT = parseInt(process.env.PORT)
-  const webapp = config.app.webApp
+const port = parseInt(process.env.PORT)
+const webapp = config.app.webApp
 
+const bootstrap = async (): Promise<void> => {
   await pg.connect(defaultDataSource)
   await setRouters(webapp, 'dist/modules/user/4.main/routers')
 
-  webapp.listen(PORT, () => {
-    console.log(`server running at http://localhost:${PORT}`)
+  webapp.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`)
   })
 }
 
