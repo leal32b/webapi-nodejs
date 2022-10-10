3c947a5e391556130350d59b63d47a9e927556b2
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2022-10-09 23:36:08 -0300

    fix: adjust mongodb connection string

--- src/core/3.infra/persistence/mongodb/data-sources/default.ts
+++ src/core/3.infra/persistence/mongodb/data-sources/default.ts
@@ -3,10 +3,12 @@ import { MongodbDataSource } from '@/core/3.infra/persistence/mongodb/client/mon
 
 const HOST = process.env.DB_MONGODB_HOST
 const PORT = process.env.DB_MONGODB_PORT
-const DATABASE = process.env.DB_POSTGRES_DATABASE
+const DATABASE = process.env.DB_MONGODB_DATABASE
 const USERNAME = process.env.DB_MONGODB_USERNAME
 const PASSWORD = process.env.DB_MONGODB_PASSWORD
-const CONNECTION_STRING = `mongodb://${USERNAME}:${PASSWORD}@${HOST}:${PORT}`
+const CONNECTION_STRING = process.env.NODE_ENV === 'development'
+  ? `mongodb://${USERNAME}:${PASSWORD}@${HOST}:${PORT}`
+  : `mongodb+srv://${USERNAME}:${PASSWORD}@${HOST}/?retryWrites=true&w=majority`
 
 export const defaultDataSource: MongodbDataSource = {
   name: 'default',
