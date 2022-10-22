a5b4af865926d838b4dcd2f95639e59d2368be3f
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2022-10-22 00:39:34 -0300

    fix: adjust makePostgresFixtures name

--- src/core/4.main/setup/fixtures/index.ts
+++ src/core/4.main/setup/fixtures/index.ts
@@ -1,6 +1,6 @@
 import { DatabaseFixture } from '@/core/3.infra/persistence/database-fixture'
 import { makeMongodbFixtures } from '@/core/4.main/setup/fixtures/make-mongodb-fixtures'
-import { makePostgresFactories } from '@/core/4.main/setup/fixtures/make-postgres-fixtures'
+import { makePostgresFixtures } from '@/core/4.main/setup/fixtures/make-postgres-fixtures'
 
 const PERSISTENCE = process.env.PERSISTENCE
 
@@ -9,7 +9,7 @@ export type DatabaseFixtures = {
 }
 
 const fixtureChoices: { [key: string]: DatabaseFixtures } = {
-  postgres: makePostgresFactories,
+  postgres: makePostgresFixtures,
   mongodb: makeMongodbFixtures
 }
 
