1f206671725e2040052e088efc0c9e2c68e31145
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-11-01 20:12:31 +0200

    test:  ensure return InvalidEmailError of pass  email with small name address

---
+++ test/modules/communication/0.domain/templates/email-validation.unit.test.ts
@@ -0,0 +1,28 @@
+import { EmailValidation } from '@/communication/0.domain/templates/email-validation'
+import { InvalidEmailError } from '@/core/0.domain/errors/invalid-email-error'
+
+const makeSut = function (): any {
+  const sut = EmailValidation
+  const clientParam = {
+    incorrectOne: 'jo@gmail.com',
+    incorrectTwo: 'johndoe@gmail',
+    incorrectTree: 'johndoe.com',
+    correct: 'johndoe@gmail.com'
+  }
+  return {
+    clientParam,
+    sut
+  }
+}
+
+describe('EmailValidation', () => {
+  it('Should return InvalidEmailError of pass  email with small nameAddress', () => {
+    const { sut, clientParam } = makeSut()
+    const email = clientParam.incorrectOne
+    const result = sut.build({ email })
+    expect(result.isLeft()).toBe(true)
+    expect(result.isRight()).toBe(false)
+    expect(result.value).toEqual(new InvalidEmailError('email', email))
+  })
+})
+
