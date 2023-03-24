ba90313efe99d23fa47081ac008a50004f9cf797
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2023-03-24 17:42:50 -0300

    refactor: remove domain-events

--- test/core/0.domain/base/aggregate-root.unit.test.ts
+++ test/core/0.domain/base/aggregate-root.unit.test.ts
@@ -1,14 +1,7 @@
 import { AggregateRoot } from '@/core/0.domain/base/aggregate-root'
-import { DomainEvent } from '@/core/0.domain/base/domain-event'
 import { Identifier } from '@/core/0.domain/utils/identifier'
 
-import { makeDomainEventStub } from '~/core/stubs/domain-event-stub'
-
 type PropsFake = {
-  anyKey: string
-}
-
-type PayloadFake = {
   anyKey: string
 }
 
@@ -16,62 +9,26 @@ class AggregateRootStub extends AggregateRoot<PropsFake> {
   public static create (props: any): AggregateRootStub {
     return new AggregateRootStub(props)
   }
-
-  testAddEvent (event: DomainEvent<PayloadFake>): void {
-    this.addEvent(event)
-  }
 }
 
 type SutTypes = {
   sut: AggregateRootStub
-  domainEventStub: DomainEvent<any>
 }
 
 const makeSut = (): SutTypes => {
-  const doubles = {
-    domainEventStub: makeDomainEventStub()
-  }
   const sut = AggregateRootStub.create({ anyKey: 'any_value' })
 
-  return { sut, ...doubles }
+  return { sut }
 }
 
 describe('AggregateRoot', () => {
   describe('success', () => {
-    it('adds an events', () => {
-      const { sut, domainEventStub } = makeSut()
-      sut.testAddEvent(domainEventStub)
-
-      const result = sut.events
-
-      expect(result[0]).instanceOf(DomainEvent)
-    })
-
-    it('clears the events', () => {
-      const { sut, domainEventStub } = makeSut()
-      sut.testAddEvent(domainEventStub)
-      sut.clearEvents()
-
-      const result = sut.events
-
-      expect(result).toEqual([])
-    })
-
     it('gets the id', () => {
       const { sut } = makeSut()
 
       const result = sut.id
 
       expect(result).toBeInstanceOf(Identifier)
-    })
-
-    it('gets the events', () => {
-      const { sut, domainEventStub } = makeSut()
-      sut.testAddEvent(domainEventStub)
-
-      const result = sut.events
-
-      expect(result.every(item => item instanceof DomainEvent)).toBe(true)
     })
   })
 })
